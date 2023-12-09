b0f3b9ada3940f47e7c483ae86c011ac3bcbf4a1
MSI\mifas <michal.zielonka77@gmail.com>
2023-12-09 18:16:05 +0100

    Update data (to validate)

--- src/Pb.Geo.Service/Services/GeoService.cs
+++ src/Pb.Geo.Service/Services/GeoService.cs
@@ -9,16 +9,13 @@ public interface IGeoService
 }
 public class GeoService : IGeoService
 {
-    private readonly ILogger<GeoService> _log;
     private readonly IEnumerable<Point> _points;
     private const int MaxSearchRadius = 10;
-    private const int MaxSearchResults = Int32.MaxValue;
     private const double EarthRadius = 6371;
     private const double RadianConst = Math.PI / 180;
 
-    public GeoService(ILogger<GeoService> log, IPointLoader pointLoader)
+    public GeoService(IPointLoader pointLoader)
     {
-        _log = log;
         _points = pointLoader.Points;
     }
 
@@ -38,15 +35,12 @@ public class GeoService : IGeoService
         };
     }
     
-    //TODO: Validate if returns proper values.
-    //Source: https://stackoverflow.com/questions/65256053/find-k-nearest-neighbor-in-c-sharp
     private IEnumerable<Point> GetNeighbors(Point point, IEnumerable<Point> points)
     {
         return points
             .Select(p => new { Point = p, Distance = CalculateDistanceBetweenPoints(point, p) })
             .Where(pointAndDistance => pointAndDistance.Distance <= Math.Pow(MaxSearchRadius, 2))
             .OrderBy(pointAndDistance => pointAndDistance.Distance)
-            .Take(MaxSearchResults)
             .Select(pointAndDistance => pointAndDistance.Point);
     }
     
